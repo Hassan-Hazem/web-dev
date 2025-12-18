cfe926ad697887aed94e7e086d9658f9b7f68e07
Abdelrahman Zayed <abdrahmanzayed@gmail.com>
2025-12-19 00:13:01 +0200

    Update emailService.js

--- Backend/services/emailService.js
+++ Backend/services/emailService.js
@@ -3,7 +3,6 @@ import nodemailer from 'nodemailer';
 
 
 const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
     host: 'smtp.gmail.com',
     port: 465,
     secure: true, // true for 465, false for other ports
